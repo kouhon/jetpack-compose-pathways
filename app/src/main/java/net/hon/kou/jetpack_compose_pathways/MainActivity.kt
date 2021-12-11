d727f09ce927339682fd97f4229cbbc2fd205149
kouhon <kouichi812@gmail.com>
2021-12-11 16:09:15 +0900

    tutorial-lesson-2

--- app/src/main/java/net/hon/kou/jetpack_compose_pathways/MainActivity.kt
+++ app/src/main/java/net/hon/kou/jetpack_compose_pathways/MainActivity.kt
@@ -1,13 +1,22 @@
 package net.hon.kou.jetpack_compose_pathways
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.hon.kou.jetpack_compose_pathways.ui.theme.JetpackcomposepathwaysTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,11 +24,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackcomposepathwaysTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MessageCard(msg = Message("Android", "Jetpack Compose"))
             }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
         }
     }
 }
@@ -33,6 +60,8 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackcomposepathwaysTheme {
-        Greeting("Android")
+        MessageCard(
+            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+        )
     }
 }
